ea546a23c133cac73a0574e98db23bb67830481b
tao_chen <Chentao12580@live.com>
2020-05-22 14:51:27 +0800

    make word count runable

--- SparkLearning/src/main/WordCount.scala
+++ SparkLearning/src/main/WordCount.scala
@@ -9,13 +9,18 @@ import org.apache.spark.{SparkConf, SparkContext}
 object WordCount {
   def main(args: Array[String]): Unit = {
     //设置本机Spark配置
-    val conf = new SparkConf().setAppName("wordCount").setMaster("local")
+    val conf = new SparkConf()
+      .setAppName("wordCount")
+      .setMaster("local")
     //创建Spark上下
     val sc = new SparkContext(conf)
     //从文件中获取数据
-    val input = sc.textFile("word.txt")
+    val input = sc.textFile("D:\\Code\\Moon\\SparkLearning\\src\\main\\word.txt")
     //分析并排序输出统计结果
-    input.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey((x, y) => x + y).sortBy(_._2,false).foreach(println _)
+    input.flatMap(line => line.split(" "))
+      .map(word => (word, 1)).reduceByKey((x, y) => x + y)
+      .sortBy(_._2,ascending = false)
+      .foreach(println)
   }
   }
 
